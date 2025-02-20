f3c57ce6963a4eab91dc2377a3711dd688f9223c
EdsonCasta <dedsoncasta07@gmail.com>
2025-02-20 15:49:03 -0500

    Implementación de función para agregar nombres a la lista

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friends = [];
+
+function addFriends() {
+
+    let input = document.getElementById("amigo");
+    let name = input.value;
+
+    input.value = "";
+
+    if (name === "") {
+        alert("Por favor, inserte un nombre válido.");
+        return;
+    }
+
+    friends.push(name);
+
+    let list = document.getElementById("listaAmigos");
+    let listName = document.createElement("li");
+
+    listName.innerText = name;
+    list.appendChild(listName)
+
+    console.log(friends)
+
+};
